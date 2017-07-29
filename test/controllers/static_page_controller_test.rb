56703c857fd500b812a7cdb9e18582d3938af9bd
Tim Fleming <tfleming26@hotmail.com>
2017-07-29 16:18:36 -0600

    Finish static page

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+  	@base_title = "EverSwift Computers"
+  end
+
+  test "should get root" do
+  	get root_url
+  	assert_response :success
+  end
+
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
-
 end
