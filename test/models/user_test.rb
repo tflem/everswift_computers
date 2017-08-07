177c5cea1f9b85c7a3a237999202407187ac61ce
Tim Fleming <tfleming26@hotmail.com>
2017-08-07 17:01:48 -0600

    Add email format validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,13 +20,31 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-   test "name should not be too long" do
-     @user.name = "a" * 51
-     assert_not @user.valid?
-   end
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
 
-    test "email should not be too long" do
-      @user.email = "a" * 244 + "@example.com"
-      assert_not @user.valid?
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+     assert_not @user.valid?
+  end
+
+  test "email validation should except valid addresses" do
+    valid_addresses = %w[user@example.com USER@fuu.COM A_US-CD@fuu.bar.org
+                         first.last@fuu.jp alvin+barb@yaz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
+  end
+
+  test "email validation sould reject invalid addresses" do
+  	invalid_addresses = %w[user@example,com user_at_fuu.org user.name@example.
+  	                       fuu@bar_bat.com foo@bar+bat.com foo@bar..com]
+  	invalid_addresses.each do |invalid_address|
+  	  @user.email = invalid_address
+  	  assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+  	end
+  end
 end
