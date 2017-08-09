4532ecb8f176c70468f293b4565c68088165efbb
Tim Fleming <tfleming26@hotmail.com>
2017-08-09 11:16:21 -0600

    Add gravatar for user show page

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-	@user = User.new(name: "Example User", email: "user@example.com",
-                   password: "fuuubarr", password_confirmation: "fuuubarr")
+	  @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "fuuubarr", password_confirmation: "fuuubarr")
   end
 
   test "should be valid" do
