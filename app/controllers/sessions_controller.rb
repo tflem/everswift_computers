9c337ccf6a26b4bc942014047419c994a52dd541
Tim Fleming <tfleming26@hotmail.com>
2017-08-15 10:11:27 -0600

    Add login method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     # Log user in and redirect to user show page
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       # Create error message
       flash.now[:danger] = "Invalid email/password combination"
