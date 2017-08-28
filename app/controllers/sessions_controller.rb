12af88c4c9e4a4bcacf92dc622a5be516287450c
Tim Fleming <tfleming26@hotmail.com>
2017-08-27 20:10:09 -0600

    Finish advanced login by adding remember checkbox with corresponding tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     # Log user in and redirect to user show page
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # Create error message
       flash.now[:danger] = "Invalid email/password combination"
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,4 +35,42 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "password" } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to login_url
+    # Simulate user clicking logout in second window
+    delete logout_path
+    follow_redirect!
+    assert_template "sessions/new"
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remember functionality" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remember functionality" do
+    # Log in to set cookie
+    log_in_as(@user, remember_me: '1')
+    # Log in again to verify cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
+
+  
