30bc5dbcd0ce8c5cdec39f55081cc6a5c47b2eec
Tim Fleming <tfleming26@hotmail.com>
2017-09-06 09:49:31 -0600

    Add index action to list all users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,4 +24,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "should redirect to login when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:patrick)
+  end
+
   test "layout links" do
     get root_path
     assert_template "static_page/home"
@@ -10,7 +14,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "section#services"
     assert_select "section#products"
     assert_select "section#about"
+    get login_path
     get signup_path
     assert_select "title", full_title("Sign Up")
+    log_in_as(@user)
+    get users_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
