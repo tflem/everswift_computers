db592c42a2b9c1547560e0ec6150012bf7a0e2e9
Tim Fleming <tfleming26@hotmail.com>
2017-09-23 16:44:47 -0600

    Update seed data and fixtures with activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
 User.create!(name:  "Example User",
-email: "furrealfriends@hotmail.com",
-password:              "foobaration",
-password_confirmation: "foobaration",
-admin: true)
+             email: "furrealfriends@hotmail.com",
+             password:              "foobaration",
+             password_confirmation: "foobaration",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@ff.com"
 password = "password"
-User.create!(name:  name,
-  email: email,
-  password:              password,
-  password_confirmation: password)
+User.create!(name: name,
+            email: email,
+            password:              password,
+            password_confirmation: password,
+            activated: true,
+            activated_at: Time.zone.now)
 end
