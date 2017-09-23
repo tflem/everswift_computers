1cb52353e1ee3e5af7cd949f0a8863c7534a53cc
Tim Fleming <tfleming26@hotmail.com>
2017-09-23 16:59:41 -0600

    Refactor email downcase method

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
 
   # Converts email to lowercase
   def downcase_email
-    self.email = email.downcase
+    email.downcase!
   end
 
   # Creates and assigns activation token and digest
