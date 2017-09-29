f1548a0afe1c7f859baba970c85713226f934580
Tim Fleming <tfleming26@hotmail.com>
2017-09-29 08:50:56 -0600

    Add edit action to activate accounts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,25 @@
 class SessionsController < ApplicationController
 
-  def new        
+  def new
   end
 
   def create
     # Log user in and redirect to user show page
     @user = User.find_by(email: params[:session][:email].downcase)
     if  @user && @user.authenticate(params[:session][:password])
-      log_in @user      
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
-    else
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
       # Create error message
-      flash.now[:danger] = "Invalid email/password combination"
+        message = "Account Not Activated. "
+        message += "Check Email for an Activation Link."
+        flash[:warning] = message
+        redirect_to login_url
+      end
+    else
+      flash.now[:danger] = "Invalid Email/Password Combination"
       render "new"
     end
   end
--- app/controllers/user_account_activations_controller.rb
+++ app/controllers/user_account_activations_controller.rb
@@ -1,2 +1,16 @@
 class UserAccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account Activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Account Activation Link Invalid"
+      redirect_to login_url
+    end
+  end
 end
