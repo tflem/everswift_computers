0fd009bb9bb8c87c4f667bad12d1baf0a2fcfe1f
Tim Fleming <tfleming26@hotmail.com>
2017-09-30 14:16:37 -0600

    Add integration tests for /users and /users/:id

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin      = users(:patrick)
     @non_admin  = users(:dash)
+    @non_activated_user = users(:paws)
   end
 
   test "index as admin, which includes pagination and delete links" do
@@ -14,13 +15,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select "div.pagination", count: 2
     first_users_page = User.paginate(page: 1)
     first_users_page.each do |user|
-      assert_select "a[href=?]", user_path(user), text: user.name
-      unless user == @admin
-        assert_select "a[href=?]", user_path(user), text: "delete"
+      if user == @non_activated_user
+        true
+      else
+        assert_select "a[href=?]", user_path(user), text: user.name
+          unless user == @admin
+            assert_select "a[href=?]", user_path(user), text: "delete"
+          end
       end
     end
       assert_difference "User.count", -1 do
-        delete user_path(@non_admin)    
+        delete user_path(@non_admin)
     end
   end
 
@@ -28,5 +33,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select "a", text: "delete", count: 0
+  end
+
+  test "should only show activated users" do
+    log_in_as(@non_admin)
+    get user_path(@non_activated_user)
+    assert_redirected_to login_url
   end
 end
