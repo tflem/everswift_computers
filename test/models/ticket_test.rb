9496983a4a55170c6ce918a1395b78b4fa791cb0
Tim Fleming <tfleming26@hotmail.com>
2017-10-14 14:32:24 -0600

    Add tests for ticket model message validations

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -15,4 +15,14 @@ class TicketTest < ActiveSupport::TestCase
     @ticket.user_id = nil
     assert_not @ticket.valid?
   end
+
+  test "message should be present" do
+    @ticket.message = "   "
+    assert_not @ticket.valid?
+  end
+
+  test "message should be at most 140 characters" do
+    @ticket.message = "a" * 141
+    assert_not @ticket.valid?
+  end
 end
