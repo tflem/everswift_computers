561b8d6164e8ade21c50b4a4965e5b1c01bbfd08
Tim Fleming <tfleming26@hotmail.com>
2017-10-14 13:48:32 -0600

    Add test for validity of a new ticket

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:patrick)
+    @ticket = Ticket.new(message: "Cannot restart PC", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @ticket.valid?
+  end
+
+  test "user id should be present" do
+    @ticket.user_id = nil
+    assert_not @ticket.valid?
+  end
 end
