06dc7a4f5263e20624cde53da042698ee20786bb
Tim Fleming <tfleming26@hotmail.com>
2017-10-31 10:21:39 -0600

    Add authorization to tickets controller actions

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,15 @@
 class TicketsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :edit, :update]
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
