ac6329cb688f60c684aa941d34258c1fddff8e86
Tim Fleming <tfleming26@hotmail.com>
2017-10-31 10:17:54 -0600

    Add authorization test for tickets controller

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @ticket = tickets(:one)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference "Ticket.count" do
+      post tickets_path, params: { ticket: { message: "I am missing a stick of RAM" } }
+    end
+    assert_redirected_to login_url
+  end
 end
