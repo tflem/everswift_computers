fdbd0f2cdc7e4eb8d01300d2d531f02f95846408
Tim Fleming <tfleming26@hotmail.com>
2017-11-04 00:28:43 -0600

    Add title, category, and status validations to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,8 @@ class Ticket < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+  validates :category, presence: true, on: [:sales, :technical]
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :status, presence: true
   validates :message, presence: true, length: { maximum: 150 }
 end
