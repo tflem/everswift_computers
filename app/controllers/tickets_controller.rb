4e74b3d0227155afd92399d5714ff2be9550c59b
Tim Fleming <tfleming26@hotmail.com>
2017-11-04 00:25:59 -0600

    Add new and create actions to ticket controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,14 +2,28 @@ class TicketsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :update]
 
   def new
+    @ticket = current_user.tickets.build if logged_in?
   end
 
   def create
+    @ticket = current_user.tickets.build(ticket_params)
+    if @ticket.save
+      flash[:success] = "Your Ticket Has Been Created."
+      redirect_to current_user
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:category, :title, :status, :message)
   end
 end
