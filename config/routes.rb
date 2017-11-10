ac93f6dacc18b5da77b989eb2c039c47d8d90604
Tim Fleming <tfleming26@hotmail.com>
2017-11-09 23:28:04 -0600

    Add ticket routes responding to GET and POST requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root   'static_page#home'
-  get    '/signup', to: 'users#new'
-  post   '/signup', to: 'users#create'
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get    '/tickets', to: 'tickets#new'
+  post   '/tickets', to: 'tickets#create'
   resources :users
   resources :user_account_activations, only: [:edit]
   resources :password_resets,          only: [:new, :create, :edit, :update]
