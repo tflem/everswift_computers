36c8faa9aae084c2cb33f4b90507c2dce6b89584
Tim Fleming <tfleming26@hotmail.com>
2017-11-09 23:11:26 -0600

    Remove unnecessary syntax for category validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :category, presence: true, on: [:sales, :technical]
+  validates :category, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :status, presence: true
   validates :message, presence: true, length: { maximum: 150 }
