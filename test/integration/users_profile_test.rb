a667f428fe150281d0f9e458818348545dcfc4cf
Tim Fleming <tfleming26@hotmail.com>
2017-11-11 16:45:23 -0600

    Finish create new ticket functionality

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -12,11 +12,10 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_template "users/show"
     assert_select "title", full_title(@user.name)
     assert_select "h1", text: @user.name
-    assert_select "h1>img.gravatar"
     assert_match @user.tickets.count.to_s, response.body
     assert_select "div.pagination", count: 2
     @user.tickets.paginate(page: 1).each do |ticket|
-      assert_match ticket.message, response.body
+      assert_match ticket.message, response.body      
     end
   end
 end
