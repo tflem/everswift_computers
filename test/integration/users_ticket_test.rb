ac96c0dc8d0706ab14852008e892bdc064d2120b
Tim Fleming <tfleming26@hotmail.com>
2017-11-11 15:50:03 -0600

    Add test for valid ticket submission

--- test/integration/users_ticket_test.rb
+++ test/integration/users_ticket_test.rb
@@ -19,4 +19,18 @@ class UsersTicketTest < ActionDispatch::IntegrationTest
    assert_select "div#error_explanation"
    assert_select "div.field_with_errors"
  end
+
+ test "valid ticket information" do
+   log_in_as(@current_user)
+   get tickets_path
+   assert_difference "Ticket.count", 1 do
+     post tickets_path, params: { ticket: { category: "Sales",
+                                            title:    "Missing RAM",
+                                            status:   "Open",
+                                            message:  "Bought 2, Received 1" } }
+   end
+   follow_redirect!
+   assert_template "users/show"
+   assert_not flash.empty?
+ end
 end
