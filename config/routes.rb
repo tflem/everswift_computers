87c74d28e5fe3958c1a6ad6797ec3d19ddb41db2
Tim Fleming <tfleming26@hotmail.com>
2017-12-02 03:36:56 -0600

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   root   'static_page#home'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  get    '/tickets/new', to: 'tickets#new'
-  post   '/tickets', to: 'tickets#create'
-  get    '/tickets/:id/', to: 'tickets#edit'
+  get    '/signup',           to: 'users#new'
+  post   '/signup',           to: 'users#create'
+  get    '/login',            to: 'sessions#new'
+  post   '/login',            to: 'sessions#create'
+  delete '/logout',           to: 'sessions#destroy'
   resources :users
   resources :user_account_activations, only: [:edit]
   resources :password_resets,          only: [:new, :create, :edit, :update]
-  resources :tickets,                  only: [:new, :create, :edit, :update]
+  resources :tickets,                  only: [:new, :create, :edit, :update,
+                                              :destroy]  
 end
