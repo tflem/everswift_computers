8868a08c5c8b38798f50ec469e4e806716db337e
Tim Fleming <tfleming26@hotmail.com>
2017-12-10 02:33:16 -0600

    Add method to restrict access to user profile pages by non admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:index, :destroy]
+  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: [:index, :destroy]
+  before_action :restrict_access, only: :show
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
@@ -64,5 +65,11 @@ class UsersController < ApplicationController
     # Confirms admin user
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def restrict_access
+      if current_user.id != params[:id].to_i
+        redirect_to current_user
+      end
     end
 end
